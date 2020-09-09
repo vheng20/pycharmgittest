54c418312ab3a9f6e72fa6f746584b8bb82f7513
vhenghpwin <vheng@163.com>
2020-09-09 18:03:48 +0800

    liststudy2

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 print("hello my friends!")
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
-print(bicycles[1])
+print(bicycles[-2])
 print(bicycles.pop(2))
 print(bicycles)
+t = []
+print(t[0])
